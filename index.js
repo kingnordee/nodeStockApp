596388135b0a3ad730187edec87698f1e2353719
King Nordee <kingnord@buffalo.edu>
2020-06-06 00:33:15 -0400

    added css styling

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const expresshandlebars = require('express-handlebars');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
-
-
 //set handlebars middleware
 app.engine('handlebars', expresshandlebars());
 app.set('view engine', 'handlebars');
 
+
+// pk_6a0b25b8abf548dca74da17a39044606
 app.get('/', (req, res) => {
     res.render('home', {
         stuff: "Kinging /"
@@ -28,6 +28,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 //set port to listen on
 app.listen( PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
-})
-
-// console.log("Kinging MajorLeaguely");
+})//END OF PROGRAM
